5defdc5024015f30438b27c6454384501149a3b6
navyCK <ckck81026@gmail.com>
2020-12-28 15:07:48 +0900

    시간 추가
    시작 버튼 누르면 일시정지 버튼으로 변경, 색 변경
    
    리셋 버튼 누르면 시간 지나고 있을 때도 일시 정지 → 시작 버튼으로 변경
    
    랩 버튼 추가하여 등수 매기기 리스트 추가
    
    툴팁 변경

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:stop_watch_timer/stop_watch_timer.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,11 +9,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
       home: MyHomePage(title: '스톱워치'),
     );
   }
@@ -28,12 +24,26 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  final _isHours = true;
+  final StopWatchTimer _stopWatchTimer = StopWatchTimer(
+
+  );
+
+  final _scrollController = ScrollController();
+
+  Icon _icon = Icon(Icons.play_arrow,);
+  Color _color = Colors.blue;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() async {
+    super.dispose();
+    await _stopWatchTimer.dispose();
   }
 
   @override
@@ -46,15 +56,90 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            Padding(
+              padding: const EdgeInsets.only(bottom: 0),
+              child: StreamBuilder<int>(
+                stream: _stopWatchTimer.rawTime,
+                initialData: _stopWatchTimer.rawTime.value,
+                builder: (context, snap) {
+                  final value = snap.data;
+                  final displayTime =
+                  StopWatchTimer.getDisplayTime(value, hours: _isHours);
+                  return Column(
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.all(8),
+                        child: Text(
+                          displayTime,
+                          style: const TextStyle(
+                              fontSize: 40,
+                              fontFamily: 'Helvetica',
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+
+                    ],
+                  );
+                },
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+
             SizedBox(
-              height: 300,
+              height: 10,
+            ),
+
+            Container(
+              height: 330,
+              margin: const EdgeInsets.all(8),
+              child: StreamBuilder<List<StopWatchRecord>>(
+                stream: _stopWatchTimer.records,
+                initialData: _stopWatchTimer.records.value,
+                builder: (context, snap) {
+                  final value = snap.data;
+                  if (value.isEmpty) {
+                    return Container();
+                  }
+                  Future.delayed(const Duration(milliseconds: 100), () {
+                    _scrollController.animateTo(
+                        _scrollController.position.maxScrollExtent,
+                        duration: const Duration(milliseconds: 200),
+                        curve: Curves.easeOut);
+                  });
+                  print('Listen records. $value');
+                  return ListView.builder(
+                    controller: _scrollController,
+                    scrollDirection: Axis.vertical,
+                    itemBuilder: (BuildContext context, int index) {
+                      final data = value[index];
+                      return Column(
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.all(8),
+                            child: Text(
+                              '${index + 1}등 \t ${data.displayTime}',
+                              style: const TextStyle(
+                                  fontSize: 17,
+                                  fontFamily: 'Helvetica',
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          const Divider(
+                            height: 1,
+                          )
+                        ],
+                      );
+                    },
+                    itemCount: value.length,
+                  );
+                },
+              ),
+            ),
+
+
+
+
+            SizedBox(
+              height: 30,
             ),
             Padding(
               padding: const EdgeInsets.all(16.0),
@@ -63,27 +148,50 @@ class _MyHomePageState extends State<MyHomePage> {
                   Align(
                     alignment: Alignment.bottomLeft,
                     child: FloatingActionButton(
-                      onPressed: _incrementCounter,
-                      tooltip: 'Increment',
+                      tooltip: '초기화',
                       child: Icon(Icons.refresh),
                       backgroundColor: Colors.red,
+                      onPressed: () {
+                        setState(() {
+                          _icon = Icon(Icons.play_arrow);
+                          _color = Colors.blue;
+                        });
+                        _stopWatchTimer.onExecute.add(StopWatchExecute.reset);
+                      },
                     ),
                   ),
                   Align(
                     alignment: Alignment.bottomCenter,
                     child: FloatingActionButton(
-                      onPressed: _incrementCounter,
-                      tooltip: 'Increment',
-                      child: Icon(Icons.play_arrow),
+                      tooltip: '시작 / 일시정지',
+                      child: _icon,
+                      backgroundColor: _color,
+                      onPressed: () {
+                        setState(() {
+                          if (_stopWatchTimer.isRunning) {
+                            _icon = Icon(Icons.play_arrow);
+                            _color = Colors.blue;
+                            _stopWatchTimer.onExecute.add(StopWatchExecute.stop);
+                          } else {
+                            _icon = Icon(Icons.pause);
+                            _color = Colors.grey;
+                            _stopWatchTimer.onExecute.add(StopWatchExecute.start);
+                          }
+                        });
+
+
+                      },
                     ),
                   ),
                   Align(
                     alignment: Alignment.bottomRight,
                     child: FloatingActionButton(
-                      onPressed: _incrementCounter,
-                      tooltip: 'Increment',
+                      tooltip: '기록 추가',
                       child: Icon(Icons.add_alarm),
                       backgroundColor: Colors.green,
+                      onPressed: () {
+                        _stopWatchTimer.onExecute.add(StopWatchExecute.lap);
+                      },
                     ),
                   ),
                 ],
